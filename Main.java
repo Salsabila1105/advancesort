cf254c48f4b5a3cd6fb9359c9640db999074ca16
Salsabila1105 <>
2021-05-23 12:08:40 +0000

    variabel

--- Main.java
+++ Main.java
@@ -3,12 +3,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main (String [] args) {
-        int data[] = {11,5,21,29,10,9,17,15,32,};
+        int data[] = {11, 5,  21,  29, 10, 9, 17, 15, 32,};
+
+        long startTime = System.nanoTime();
         int min = data[0];
         int index = 0;
 
-        for(int j=0;j<data.length-1;j++) {
-            for (int i=j;i<data.length;i++) {
+        for(int j=0; j<data.length-1; j++) {
+            for (int i=j; i<data.length; i++) {
                 if (data[i] < min) {
                     min = data[i];
                     index = i;
@@ -21,6 +23,9 @@ public class Main {
             }
             min = data[j+1];
         }
-        System.out.println(Arrays.toString(data));
+        long endTime = System.nanoTime();
+
+        System.out.println("Hasil sorting : " + Arrays.toString(data));
+        System.out.println("waktu sorting : " + (endTime - startTime) + " Nano Detik");
     }
 }
